a39f4659dfd78e32533820a053eab65e4cbdf3cf
PallavSharma9 <pallavsharma0202@gmail.com>
2025-11-18 06:55:01 +0530

    Adding components and fanta.css file

---
+++ src/components/pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <section id="welcome">
+    <div class="benefits">
+      <h2>Complete this program<br />if you want</h2>
+      <div>
+        <p>✅ Follow a simple program with proven results.</p>
+        <p>✅ Get fit, healthy, strong and absolutely shredded.</p>
+        <p>✅ Learn more about gym, training and techniques</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
