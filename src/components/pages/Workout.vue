b7f267e7c570ca3506aeab13e06dc7fbf5ebefd7
PallavSharma9 <pallavsharma0202@gmail.com>
2025-11-19 19:34:25 +0530

    Adding Excercise Description

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -1,12 +1,24 @@
 <script setup>
-import { workoutProgram } from "../../utils";
+import { exerciseDescriptions, workoutProgram } from "../../utils";
 import Portal from "../Portal.vue";
 const selectedWorkout = 4;
 const { workout, warmup } = workoutProgram[selectedWorkout];
+
+let selectedExercise = "Around the worlds";
+const exerciseDescription = exerciseDescriptions[selectedExercise];
 </script>
 
 <template>
-  <Portal> </Portal>
+  <Portal v-if="selectedExercise">
+    <div class="exercise-description">
+      <h3>{{ selectedExercise }}</h3>
+      <div>
+        <small>Description</small>
+        <p>{{ exerciseDescription }}</p>
+      </div>
+      <button>Close <i class="fa-solid fa-xmark"></i></button>
+    </div>
+  </Portal>
   <section id="workout-card">
     <div class="plan-card card">
       <div class="plan-card-header">
@@ -134,6 +146,21 @@ const { workout, warmup } = workoutProgram[selectedWorkout];
 }
 
 .workout-btns button i {
+  padding-left: 0.5rem;
+}
+
+.exercise-description {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+}
+
+.exercise-description h3 {
+  text-transform: capitalize;
+}
+
+.exercise-description button i {
   padding-left: 0.5rem;
 }
 </style>
