94c2a1c1cf5cf6816c62a977f31a6aa843e8ec2b
PallavSharma9 <pallavsharma0202@gmail.com>
2025-12-04 11:37:38 +0530

    Adding fully working WebApp

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -45,7 +45,11 @@ function handleCloseModal() {
       <div class="plan-card-header">
         <p>
           Day
-          {{ selectedWorkout < 9 ? "0" + selectedWorkout : selectedWorkout }}
+          {{
+            selectedWorkout < 9
+              ? "0" + (selectedWorkout + 1)
+              : selectedWorkout + 1
+          }}
         </p>
         <i class="fa-solid fa-dumbbell"></i>
       </div>
